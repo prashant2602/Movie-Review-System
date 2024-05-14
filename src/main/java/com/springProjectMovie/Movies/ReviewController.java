e03adedd128af59bebea801563d3624aaa6b27f8
prashant2602 <prashantshrivastava2607@gmail.com>
2024-05-14 18:55:41 +0530

    Add existing project files prior to the push to GitHub.

---
+++ src/main/java/com/springProjectMovie/Movies/ReviewController.java
@@ -0,0 +1,22 @@
+package com.springProjectMovie.Movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+
+    @Autowired
+    public ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String,String> payload) {
+        return
+     new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"),payload.get("imdbId")),HttpStatus.CREATED);
+    }
+}
